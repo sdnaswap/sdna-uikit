35ddbd5bae074e7b3421ff04be9de4f9a702ce05
Literally Collective <bluedragon.cstar@outlook.com>
2021-06-09 16:36:25 -0300

    Feat: update social links

--- dist/index.cjs.js
+++ dist/index.cjs.js
@@ -3462,65 +3462,66 @@ var socials = [
     {
         label: "Telegram",
         icon: "TelegramIcon",
-        items: [
-            {
-                label: "English",
-                href: "https://t.me/pancakeswap",
-            },
-            {
-                label: "Bahasa Indonesia",
-                href: "https://t.me/PancakeSwapIndonesia",
-            },
-            {
-                label: "中文",
-                href: "https://t.me/PancakeSwap_CN",
-            },
-            {
-                label: "Tiếng Việt",
-                href: "https://t.me/PancakeSwapVN",
-            },
-            {
-                label: "Italiano",
-                href: "https://t.me/pancakeswap_ita",
-            },
-            {
-                label: "русский",
-                href: "https://t.me/pancakeswap_ru",
-            },
-            {
-                label: "Türkiye",
-                href: "https://t.me/pancakeswapturkiye",
-            },
-            {
-                label: "Português",
-                href: "https://t.me/PancakeSwapPortuguese",
-            },
-            {
-                label: "Español",
-                href: "https://t.me/PancakeswapEs",
-            },
-            {
-                label: "日本語",
-                href: "https://t.me/pancakeswapjp",
-            },
-            {
-                label: "Français",
-                href: "https://t.me/pancakeswapfr",
-            },
-            {
-                label: "Announcements",
-                href: "https://t.me/PancakeSwapAnn",
-            },
-            {
-                label: "Whale Alert",
-                href: "https://t.me/PancakeSwapWhales",
-            },
-        ],
+        href: "https://t.me/superdnadefi",
+        // items: [
+        //   {
+        //     label: "English",
+        //     href: "https://t.me/pancakeswap",
+        //   },
+        //   {
+        //     label: "Bahasa Indonesia",
+        //     href: "https://t.me/PancakeSwapIndonesia",
+        //   },
+        //   {
+        //     label: "中文",
+        //     href: "https://t.me/PancakeSwap_CN",
+        //   },
+        //   {
+        //     label: "Tiếng Việt",
+        //     href: "https://t.me/PancakeSwapVN",
+        //   },
+        //   {
+        //     label: "Italiano",
+        //     href: "https://t.me/pancakeswap_ita",
+        //   },
+        //   {
+        //     label: "русский",
+        //     href: "https://t.me/pancakeswap_ru",
+        //   },
+        //   {
+        //     label: "Türkiye",
+        //     href: "https://t.me/pancakeswapturkiye",
+        //   },
+        //   {
+        //     label: "Português",
+        //     href: "https://t.me/PancakeSwapPortuguese",
+        //   },
+        //   {
+        //     label: "Español",
+        //     href: "https://t.me/PancakeswapEs",
+        //   },
+        //   {
+        //     label: "日本語",
+        //     href: "https://t.me/pancakeswapjp",
+        //   },
+        //   {
+        //     label: "Français",
+        //     href: "https://t.me/pancakeswapfr",
+        //   },
+        //   {
+        //     label: "Announcements",
+        //     href: "https://t.me/PancakeSwapAnn",
+        //   },
+        //   {
+        //     label: "Whale Alert",
+        //     href: "https://t.me/PancakeSwapWhales",
+        //   },
+        // ],
     },
     {
         label: "Twitter",
         icon: "TwitterIcon",
-        href: "https://twitter.com/pancakeswap",
+        href: "https://twitter.com/Superdnadefi",
     },
 ];
 var MENU_HEIGHT = 64;
@@ -3673,11 +3674,18 @@ var SocialLinks = function () { return (React__default['default'].createElement(
     var Icon = Icons$1[social.icon];
     var iconProps = { width: "24px", color: "textSubtle", style: { cursor: "pointer" } };
     var mr = index < socials.length - 1 ? "24px" : 0;
-    if (social.items) {
-        return (React__default['default'].createElement(Dropdown, { key: social.label, position: "top", target: React__default['default'].createElement(Icon, __assign({}, iconProps, { mr: mr })) }, social.items.map(function (item) { return (
-        // <Link external key={item.label} href={item.href} aria-label={item.label} color="textSubtle">
-        React__default['default'].createElement(Link, { external: true, key: item.label, href: "#", "aria-label": item.label, color: "textSubtle" }, item.label)); })));
-    }
+    // if (social.items) {
+    //   return (
+    //     <Dropdown key={social.label} position="top" target={<Icon {...iconProps} mr={mr} />}>
+    //       {social.items.map((item) => (
+    //         // <Link external key={item.label} href={item.href} aria-label={item.label} color="textSubtle">
+    //         <Link external key={item.label} href="#" aria-label={item.label} color="textSubtle">
+    //           {item.label}
+    //         </Link>
+    //       ))}
+    //     </Dropdown>
+    //   );
+    // }
     return (
     // <Link external key={social.label} href={social.href} aria-label={social.label} mr={mr}>
     React__default['default'].createElement(Link, { external: true, key: social.label, href: "#", "aria-label": social.label, mr: mr },
